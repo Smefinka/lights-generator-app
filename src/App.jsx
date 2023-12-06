e6e28d924ddfca696d027201eb5e72ed88eeff4c
Smefinka <smefinka1@gmail.com>
2023-12-06 12:00:31 +0100

    wow

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Counter from "./components/Counter";
 function App() {
   return (<>
    
-    <nav className="nav">
-    Lights generator
+    <nav className="nav1">
+    Lights1 generator
     <p style={{paddingRight: '70px'}}>0 bulbs</p>
     <Counter text="row" count="0" /> 
     <Counter text="columns" count="0" />
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,27 +3,25 @@ import IconMinus from "../icons/IconMinus";
 import styles from "../components/Counter.module.css"
 import { useState } from "react";
 function Counter(props){
-    const [value, setValue] = useState(0);
-    function handleDecreament() {
-        if(value <= 0){
-            return setValue(0);
+    const [value1, setValue1] = useState(0);
+    function handleDecreament1() {
+        if(value1 <= 0){
+            return setValue1(0);
         }
-        setValue(value-1);
+        setValue1(value1-1);
     }
-    function handleIncreament(){
-        setValue(value+1)
+    function handleIncreament1(){
+        setValue1(value1+1)
     }
     return(
         <>
   <div className={styles.nav}>
        <p>
-        {value} {props.text} 
+        {value1} {props.text} 
         </p>
        <nav>
-        
-       <IconMinus onClick={handleDecreament} /> 
-
-       <IconAdd  onClick={handleIncreament}/>
+       <IconMinus onClick={handleDecreament1} /> 
+       <IconAdd  onClick={handleIncreament1}/>
        </nav>
       
        </div>
