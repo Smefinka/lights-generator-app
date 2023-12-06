c281161393e686b2e2c4363029d729fdedfaba41
Smefinka <smefinka1@gmail.com>
2023-12-06 11:45:57 +0100

    create counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
-
+import Counter from "./components/Counter";
 function App() {
-  return <div>Lights generator</div>;
+  return (<>
+   
+    <nav className="nav">
+    Lights generator
+    <p style={{paddingRight: '70px'}}>0 bulbs</p>
+    <Counter text="row" count="0" /> 
+    <Counter text="columns" count="0" />
+       </nav>
+    
+    </>);
 }
 
 export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,33 @@
+import IconAdd from "../icons/IconAdd";
+import IconMinus from "../icons/IconMinus";
+import styles from "../components/Counter.module.css"
+import { useState } from "react";
+function Counter(props){
+    const [value, setValue] = useState(0);
+    function handleDecreament() {
+        if(value <= 0){
+            return setValue(0);
+        }
+        setValue(value-1);
+        console.log('hi')
+    }
+    function handleIncreament(){
+        setValue(value+1)
+    }
+    return(
+        <>
+  <div className={styles.nav}>
+       <p>
+        {value} {props.text} 
+        </p>
+       <nav>
+       <IconMinus onClick={handleDecreament} /> 
+       <IconAdd  onClick={handleIncreament}/>
+       </nav>
+      
+       </div>
+  
+        </>
+    )
+}
+export default Counter;
